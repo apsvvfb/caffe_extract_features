d516a79daa06db0ddb6d20b67e23636cd35a6652
apsvvfb <apsvvfb@gmail.com>
2016-06-19 19:45:11 +0900

    Update feature_extractor.py

--- feature_extractor.py
+++ feature_extractor.py
@@ -13,11 +13,6 @@ outputfile='output.txt'
 batchsize=10
 j=-1
 
-# set display defaults
-plt.rcParams['figure.figsize'] = (10, 10)        # large images
-plt.rcParams['image.interpolation'] = 'nearest'  # don't interpolate: show square pixels
-plt.rcParams['image.cmap'] = 'gray'  # use grayscale output rather than a (potentially misleading) color heatmap
-
 import sys
 sys.path.insert(0, caffe_root + 'python')
 
